3d38103fb0ed215e1c38a5b5f3da729efd9b60f1
Michel-mihim <ya-mihim@yandex.ru>
2024-08-07 13:55:51 +0300

    3_no_list?

--- app/src/main/java/com/example/forecastinterfacepractise/ForecastBox.kt
+++ app/src/main/java/com/example/forecastinterfacepractise/ForecastBox.kt
@@ -3,31 +3,35 @@ package com.example.forecastinterfacepractise
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.recyclerview.widget.RecyclerView
 
 data class ForecastBox(val weekDay: String)
 
-class ForecastBoxesAdapter(
+class ForecastBoxAdapter(
     private val forecastBoxes: List<ForecastBox>
-) : RecyclerView.Adapter<ForecastBoxesAdapter.ForecastBoxViewHolder>() {
+) : RecyclerView.Adapter<ForecastBoxAdapter.ForecastBoxViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ForecastBoxViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.forecast_box_view, parent, false)
         return ForecastBoxViewHolder(view)
     }
 
-    override fun onBindViewHolder(
-        holder: ForecastBoxViewHolder,
-        position: Int
-    ) {
+    override fun onBindViewHolder(holder: ForecastBoxViewHolder, position: Int) {
         holder.bind(forecastBoxes[position])
     }
 
     override fun getItemCount() = forecastBoxes.size
 
     class ForecastBoxViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(forecastBox: ForecastBox) {
+        private val weekDayTextView: TextView
 
+        init {
+            weekDayTextView = itemView.findViewById(R.id.weekDay)
+        }
+
+        fun bind(forecastBox: ForecastBox) {
+            weekDayTextView.text = forecastBox.weekDay
         }
 
     }
--- app/src/main/java/com/example/forecastinterfacepractise/MainActivity.kt
+++ app/src/main/java/com/example/forecastinterfacepractise/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val week_days_list = listOf(R.string.day1, R.string.day2, R.string.day3, R.string.day4,
+            R.string.day5, R.string.day6, R.string.day7)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.forecastBoxesRecycler)
+        recyclerView.adapter = ForecastBoxAdapter(
+            forecastBoxes = List(90) {
+                ForecastBox("1")
+            }
+        )
+
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
     }
 }
